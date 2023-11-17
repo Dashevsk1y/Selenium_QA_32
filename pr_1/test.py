ccdac876408730fe8f9d22b9dd374099ba5be5ce
Dashevsk1y <96soundmedia@gmail.com>
2023-11-17 20:07:31 +0200

    pr_3.1

---
+++ pr_1/test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+ 
+driver = webdriver.Chrome()
+driver.get("http://www.google.com")
+ 
+elem = driver.find_element_by_name("q")
+elem.send_keys("Hello WebDriver!")
+elem.submit()
+ 
+print(driver.title)
